41ed09afd846e8a166579e4ce6f18ffbed6c3cb5
achmadrendra26 <achmadrendra26@gmail.com>
2021-10-07 02:08:56 +0700

    Update Front End (HomePage)

---
+++ resources/views/home.blade.php
@@ -0,0 +1,307 @@
+<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Adopt And Find Your Lost Pet | Our Pet's</title>
+      <link rel="stylesheet" type="text/css" href="css/styles.css"/>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    </head>
+    <body>
+        <!-- NAVBAR -->
+       <section class="h-100 w-100" style="box-sizing: border-box; background-color: #004AAD">
+    <nav class="navbar-1-3 navbar navbar-expand-lg navbar-dark p-4 px-md-4">
+      <div class="container">
+        <a class="navbar-brand" href="home">
+          <img src="Images/logo putih.png" width="91.84" height="50" alt="LOGO">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02"
+          aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link px-md-4 active" aria-current="page" href="Lost">Lost Pet</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link px-md-4" href="Adopt">Adoption</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link px-md-4" href="Clinic">Clinic</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link px-md-4" href="Tips">Tips & Trick</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link px-md-4" href="About">About</a>
+            </li>
+          </ul>
+          <div class="d-flex">
+            <a class="btn btn-login text-white" href="Login">Login</a>
+            <a class="btn btn-register btn-fill text-white" href="#">Register</a>
+          </div>
+         
+        </div>
+      </div>
+    </nav>
+  </section>
+   <!--Akhir NAVBAR  -->
+   <!-- HERO -->
+   <section class="hero">
+       <div class="text-hero">
+            <h1 class = "heading-text">
+                Make a New
+            </h1>
+            <h1 class="accent-text">
+                FRIEND
+            </h1>
+            <h1 class="heading-text2">
+                With Us
+            </h1>
+            <div class="p-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </div>
+            <button class="btn btn-get-started text-white">
+                Get Started
+            </button>
+       </div>
+       <div class="img-hero">
+           <img src="Images/Hero.svg" width="441.54" height="600">
+       </div>
+</section>
+   <!-- Akhir Hero -->
+   <!-- Card Lost Pet -->
+   <section class="lost-card">
+       <div class="container">
+           <div class="row text-center">
+               <div class="col">
+                   <h1 class="title">
+                       Lost Pet Nearby
+                   </h1>
+               </div>
+           </div>
+           <div class="row">
+               <div class="col-3">
+               <div class="card bg-dark text-white">
+                <img src="https://images.unsplash.com/photo-1548247416-ec66f4900b2e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=420&q=80" height="350" class="card-img" alt="...">
+            <div class="card-img-overlay">
+             <h5 class="card-title">Greyi</h5>
+             <p class="card-category-age">Cat | Adult</p>
+             <p class="card-distance">500 m</p>
+            </div>
+                </div>
+               </div>
+               <div class="col-3">
+               <div class="card bg-dark text-white">
+                <img src="https://media.istockphoto.com/photos/cute-kitten-licking-glass-table-with-copy-space-picture-id1293763250?b=1&k=20&m=1293763250&s=170667a&w=0&h=zcK63xxkMVX-ca0d5laTsDxauVUMH62F71MKcbqaGnI=" height="350" class="card-img" alt="...">
+            <div class="card-img-overlay">
+             <h5 class="card-title">Greyi</h5>
+             <p class="card-category-age">Cat | Adult</p>
+             <p class="card-distance">500 m</p>
+            </div>
+                </div>
+               </div>
+               <div class="col-3">
+               <div class="card bg-dark text-white">
+                <img src="https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=436&q=80" height="350" class="card-img" alt="...">
+            <div class="card-img-overlay">
+             <h5 class="card-title">Greyi</h5>
+             <p class="card-category-age">Cat | Adult</p>
+             <p class="card-distance">500 m</p>
+            </div>
+                </div>
+               </div>
+               <div class="col-3">
+               <div class="card bg-dark text-white">
+                <img src="Images/Foot.svg" height="350">
+            <div class="card-img-overlay">
+             <h5 class="card-title">Greyi</h5>
+             <p class="card-category-age">Cat | Adult</p>
+             <p class="card-distance">500 m</p>
+            </div>
+                </div>
+               </div>
+           </div>
+       </div>
+   </section>
+   <!-- Akhir Card Lost Pet -->
+   <!-- Information Adopt -->
+   <section class="Information-adopt">
+       <div class="container">
+           <div class="row text-center">
+               <div class="col">
+                   <h1 class="title-information">
+                       Planning To Adopt a Pet?
+                   </h1>
+               </div>
+           </div>
+           <div class="row text-center">
+               <div class="col">
+                   <h4 class="title-adopt text-white">
+                       Checklist for New Adopters
+                   </h4>
+                   <p class="caption text-white">
+                   Help make the transition, as smooth as possible.
+                   </p>
+                   <button class="btn-learn-more">
+                       Learn More
+                   </button>
+               </div>
+               <div class="col">
+                   <h4 class="title-adopt text-white">
+                   COVID-19 Resources
+                   </h4>
+                   <p class="caption text-white">
+                   Get the latest on adoption processes, learn how local shelters
+and rescue groups are adapting and
+find out what you can do to help dogs and cats in need right now.
+                   </p>
+                   <button class="btn-learn-more">
+                       Learn More
+                   </button>
+               </div>
+               <div class="col">
+                   <h4 class="title-adopt text-white">
+                   Pet Adoption FAQs
+                   </h4>
+                   <p class="caption text-white">
+                   Get answers to questions you
+haven't thought of.
+                   </p>
+                   <button class="btn-learn-more">
+                       Learn More
+                   </button>
+               </div>
+           </div>
+       </div>
+
+   </section>
+   <!-- Akhir Information Adopt -->
+   <!-- Adopsi Nearby -->
+   <section class="lost-card">
+       <div class="container">
+           <div class="row text-center">
+               <div class="col">
+                   <h1 class="title">
+                   Pets Available for Adoption Nearby
+                   </h1>
+               </div>
+           </div>
+           <div class="row">
+               <div class="col-3">
+               <div class="card bg-dark text-white">
+                <img src="https://images.unsplash.com/photo-1548247416-ec66f4900b2e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=420&q=80" height="350" class="card-img" alt="...">
+            <div class="card-img-overlay">
+             <h5 class="card-title">Greyi</h5>
+             <p class="card-category-age">Cat | Adult</p>
+             <p class="card-distance">500 m</p>
+            </div>
+                </div>
+               </div>
+               <div class="col-3">
+               <div class="card bg-dark text-white">
+                <img src="https://media.istockphoto.com/photos/cute-kitten-licking-glass-table-with-copy-space-picture-id1293763250?b=1&k=20&m=1293763250&s=170667a&w=0&h=zcK63xxkMVX-ca0d5laTsDxauVUMH62F71MKcbqaGnI=" height="350" class="card-img" alt="...">
+            <div class="card-img-overlay">
+             <h5 class="card-title">Greyi</h5>
+             <p class="card-category-age">Cat | Adult</p>
+             <p class="card-distance">500 m</p>
+            </div>
+                </div>
+               </div>
+               <div class="col-3">
+               <div class="card bg-dark text-white">
+                <img src="https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=436&q=80" height="350" class="card-img" alt="...">
+            <div class="card-img-overlay">
+             <h5 class="card-title">Greyi</h5>
+             <p class="card-category-age">Cat | Adult</p>
+             <p class="card-distance">500 m</p>
+            </div>
+                </div>
+               </div>
+               <div class="col-3">
+               <div class="card bg-dark text-white">
+                <img src="Images/Foot.svg" height="350">
+            <div class="card-img-overlay">
+             <h5 class="card-title">Greyi</h5>
+             <p class="card-category-age">Cat | Adult</p>
+             <p class="card-distance">500 m</p>
+            </div>
+                </div>
+               </div>
+           </div>
+       </div>
+   </section>
+   <!-- Akhir Adopsi Nearby-->
+   <!-- Tips & Trick -->
+   <section class="tips-card">
+       <div class="container">
+           <div class="row text-center">
+               <div class="col">
+                   <h1 class="title text-white">
+                   Tips And Trick
+                   </h1>
+               </div>
+           </div>
+           <div class="row">
+               <div class="col-3">
+               <div class="card bg-dark text-white">
+                <img src="https://media.istockphoto.com/photos/good-dog-picture-id938105034?b=1&k=20&m=938105034&s=170667a&w=0&h=T3KyQg1aSQao7W0OQ-BluAt4q4SwmjLGSLnBOiAmk5s=" height="350" class="card-img" alt="...">
+            <div class="card-img-overlay">
+             <h5 class="card-title">Greyi</h5>
+             <p class="card-category-age">Cat | Adult</p>
+             <p class="card-distance">500 m</p>
+            </div>
+                </div>
+               </div>
+               <div class="col-3">
+               <div class="card bg-dark text-white">
+                <img src="https://media.istockphoto.com/photos/cute-kitten-licking-glass-table-with-copy-space-picture-id1293763250?b=1&k=20&m=1293763250&s=170667a&w=0&h=zcK63xxkMVX-ca0d5laTsDxauVUMH62F71MKcbqaGnI=" height="350" class="card-img" alt="...">
+            <div class="card-img-overlay">
+             <h5 class="card-title">Greyi</h5>
+             <p class="card-category-age">Cat | Adult</p>
+             <p class="card-distance">500 m</p>
+            </div>
+                </div>
+               </div>
+               <div class="col-3">
+               <div class="card bg-dark text-white">
+                <img src="https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=436&q=80" height="350" class="card-img" alt="...">
+            <div class="card-img-overlay">
+             <h5 class="card-title">Greyi</h5>
+             <p class="card-category-age">Cat | Adult</p>
+             <p class="card-distance">500 m</p>
+            </div>
+                </div>
+               </div>
+               <div class="col-3">
+               <div class="card bg-dark text-white">
+                <img src="Images/Foot.svg" height="350">
+            <div class="card-img-overlay">
+             <h5 class="card-title">Greyi</h5>
+             <p class="card-category-age">Cat | Adult</p>
+             <p class="card-distance">500 m</p>
+            </div>
+                </div>
+               </div>
+           </div>
+       </div>
+   </section>
+   <!-- Akhir Tips & Trick -->
+   <!-- Footer -->
+   <section class="footer">
+       <div class="container">
+           <div class="row">
+               <div class="col">
+               <img src="Images/logo putih.png" width="183.67" height="100" alt="" class="footer">
+               </div>
+           </div>
+           <div class="row text-center">
+               <p class="copyright text-white">Copyright © 2021 Our Pet’s. All Rights Reserved</p>
+           </div>
+       </div>
+   </section>
+   <!-- Akhir Footer -->
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+    </body>
+  </html>
