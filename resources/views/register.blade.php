a3458b19331c9693453d37caf2c99c46b51a0494
Reza Assidqi <rasidky@gmail.com>
2021-10-09 22:11:03 +0700

    last update

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -7,7 +7,7 @@
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.88.1">
-    <title>LOGIN</title>
+    <title>Register</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/sign-in/">
 
@@ -41,12 +41,18 @@
 <main class="form-signin">
   <form>
     <img class="mb-4" src="Images/logo.svg"  alt="" width="266.22" height="150">
-    <h1 class="tit-login h3 mb-3 fw-normal">Login</h1>
+    <h1 class="tit-login h3 mb-3 fw-normal">Register</h1>
 
     <div class="form-floating">
       <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
       <label for="floatingInput">Email address</label>
     </div>
+
+    <div class="form-floating">
+      <input type="text" class="form-control" id="floatingInput" placeholder="Userid">
+      <label for="floatingInput">Full Name</label>
+    </div>
+
     <div class="form-floating">
       <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
       <label for="floatingPassword">Password</label>
